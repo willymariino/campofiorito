48368be73fc4c40b148834cf2c10946092e41624
willymariino <wg.mariino@gmail.com>
2026-02-25 19:53:30 +0100

    add: event listener on cell

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const board = document.querySelector(".board")
 for (let i = 0; i < 100; i++) {
     const cell = document.createElement("div")
     cell.innerText = i + 1
-    cell.classList.add("cell")
+    cell.addEventListener("click", function (e) {
+        // cell.classList.remove("closed")
+        // cell.classList.add("open")
+        e.target.classList.remove("closed")
+        e.target.classList.add("open")
+    })
+
+
+    cell.classList.add("cell", "closed")
     board.append(cell)
 }
 
